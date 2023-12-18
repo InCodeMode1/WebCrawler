e25f1b2c7e6ad315dc8dc7f51443aa098284fbf8
Alfredo Boron <alron1984@gmail.com>
2023-12-17 19:45:53 -0800

    sorter script complete

--- sorter.js
+++ sorter.js
@@ -1,11 +1,24 @@
 
 function printReport(pages){
-    for (const page of pages){
-        console.log(`Found ${pages[page]} internal links to ${page}`)
+
+    console.log("============================Generating crawl Report============================")
+    sortedPages = sorter(pages)
+    for (const page of sortedPages){
+        console.log(`Found ${page[1]} internal links to ${page[0]}\n`)
     }
 }
 
-function sorter(){}
+function sorter(pages){
+    sortArr = Object.keys(pages).map((key) => [key, pages[key]])
+
+    sortArr.sort(function(a, b) {
+        if (a[1] > b[1]) {return -1}
+        else if (a[1] < b[1]){return 1}
+        else {return 0}
+    })
+
+    return sortArr
+}
 
 
 
